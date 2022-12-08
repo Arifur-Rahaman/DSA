0e2620a2b90a6731f4d9b006deeacfd90e8cec83
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2022-12-08 23:23:09 +0600

    pop method implemented

--- data-structures/singly-linked-list.js
+++ data-structures/singly-linked-list.js
@@ -10,7 +10,7 @@ class LinkedList {
         this.head= null
         this.tail= null
         this.length = 0
-    }
+    };
     push(val){
         const newList = new Node(val)
         if(this.head===null){
@@ -23,9 +23,28 @@ class LinkedList {
             this.length++
         }
         return this
+    };
+    pop(){
+        if(!this.head) return undefined
+        let current = this.head
+        let pre = current
+        while(current.next){
+            pre = current
+            current = current.next
+        }
+        this.tail = pre
+        this.tail.next = null
+        this.length--
+        if(this.length===0){
+            this.head = null
+            this.tail = null
+        }
+
+        return current
     }
 }
 
 list = new LinkedList()
 console.log(list.push(5))
-console.log(list.push(6))
+// list.push(6)
+console.log(list.pop())
